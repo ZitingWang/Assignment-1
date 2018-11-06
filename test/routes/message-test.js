97fdef192857a27c8f104fd27ac0c8fa0df9f728
ZITINGWANG <2264853546@qq.com>
2018-11-06 16:04:14 +0000

    Function Post and Delete in message tested successfully

--- test/routes/message-test.js
+++ test/routes/message-test.js
@@ -75,5 +75,82 @@ describe('Message', function (){
                 });
         });
     });
+    describe('POST /message', function () {
+        it('should return confirmation message and update datastore', function(done) {
+            let information = {
+                sender: 'test' ,
+                content: '123'
+            };
+            request(server)
+                .post('/message')
+                .send(information)
+                .end(function(err, res) {
+                    expect(res).to.have.status(200);
+                    expect(res.body).to.have.property('message').equal('Message sent by test Successfully Added!' );
+                    done();
+                });
+        });
+        after(function  (done) {
+            request(server)
+                .get('/message/f/bj')
+                .end(function(err, res) {
+                    expect(res).to.have.status(200);
+                    expect(res.body).to.be.a('array');
+                    expect(res.body.length).to.equal(1);
+                    let result = _.map(res.body, (inf) => {
+                        return { sender: inf.sender,
+                            content: inf.content}
+                    });
+                    expect(result).to.include( { sender : "bj", content : "This bj's test!"  } );
+                    done();
+                });
+        });
+    });
 
+    describe('DELETE /message/:sender', function () {
+        describe('DELETE /message/:sender', function () {
+            it('should return Information Successfully Deleted!', function(done) {
+                request(server)
+                    .delete('/message/test')
+                    .end(function(err, res) {
+                        expect(res).to.have.status(200);
+                        let information = res.body.message;
+                        expect(information).to.include('Message Successfully Deleted!');
+                        done();
+                    });
+            });
+            after(function  (done) {
+                request(server)
+                    .get('/message/f/123')
+                    .end(function(err, res) {
+                        expect(res).to.have.status(200);
+                        expect(res.body).to.be.empty
+                        done();
+                    });
+            });
+        });
+        describe('DELETE /message/:sender', function () {
+            it('should return message Deleted err!', function(done) {
+                request(server)
+                    .delete('/message/ly')
+                    .end(function(err, res) {
+                        expect(res).to.have.status(200);
+                        done();
+                    });
+            });
+        });
+    });
+    describe('DELETE /message', function () {
+        it('should return All of the messages Successfully Deleted!', function(done) {
+            request(server)
+                .delete('/message')
+                .end(function(err, res) {
+                    expect(res).to.have.status(200);
+                    let inf = res.body.message;
+                    expect(inf).to.include('All of the messages Successfully Deleted!');
+                    done();
+                });
+        });
+
+    });
 });
